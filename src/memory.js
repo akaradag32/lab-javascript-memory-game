fbcf92942c12c5477939c0760d01da40ae307125
Kerem Karadag <akaradag32@gmail.com>
2023-09-13 15:59:59 +0200

    halfway throuh

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,48 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
     // ... write your code here
+    if (!this.cards) {
+      return undefined;
+    }
+
+    const shuffleArray = (array) => {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    };
+
+    return shuffleArray(this.cards);
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+
+    this.pairsClicked += 1;
+
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+    }
+
+    return card1 === card2;
   }
 
   checkIfFinished() {
     // ... write your code here
+    if (this.pairsGuessed === 0 || this.pairsGuessed < this.cards.length / 2) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
